df9541facbf8c37cef5c48302f54cfb382afd1b9
Nikita Rai <rainikita595@gmail.com>
2021-09-23 00:40:26 +0530

    [Nikita] Add: login ui , data binding , and API is done

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserServiceService } from 'src/app/services/user-service.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  hide = true;
+
+  constructor(private service: UserServiceService, private router: Router) { }
+  form = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.email, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)])
+  })
+
+  ngOnInit(): void {
+  }
+  submit() {
+     console.log(this.form); 
+    if (this.form.valid) {
+      let dataLogin = {
+        "email": this.form.controls.userName.value,
+        "password": this.form.controls.password.value,
+      }
+      
+      this.service.login(dataLogin).subscribe((dataLogin: any) => {
+        console.log(dataLogin)
+        localStorage.setItem("email", dataLogin["email"]);
+        localStorage.setItem("token", dataLogin.result.accessToken);
+        this.router.navigate(['/home'])
+        
+      },
+      error => {
+        console.log(error);
+      });
+    }
+  }  
+
+}
