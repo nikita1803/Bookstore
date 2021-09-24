c7a0ce8490b160285b6f62223d17959e1e36d605
Nikita Rai <rainikita595@gmail.com>
2021-09-24 15:03:40 +0530

    [Nikita] Modify: api calling and intendation is done

--- src/app/components/sub-head-display/sub-head-display.component.ts
+++ src/app/components/sub-head-display/sub-head-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BookServiceService } from 'src/app/services/book-service.service';
 
 @Component({
   selector: 'app-sub-head-display',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-head-display.component.scss']
 })
 export class SubHeadDisplayComponent implements OnInit {
-
-  constructor() { }
+  books: Array<any> =[];
+  
+  constructor(private bookService: BookServiceService) { }
 
   ngOnInit(): void {
+    this.getData()
+    
+  }
+  getData = () => {
+    this.bookService.getBook().subscribe((data:any)=>{
+      this.books = data['result']
+      console.log(this.books) 
+      console.log(this.books.length)
+    },
+    error => {
+      console.log(error);
+    });
   }
 
 }
