f57937dc60cdbf4f83fb4d388de570e731210045
Nikita Rai <rainikita595@gmail.com>
2021-09-29 03:23:49 +0530

    [Nikita] Modify: storing the wishlist books

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookServiceService } from 'src/app/services/book-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
-  constructor() { }
+  token: any
+  wishlistBook: Array<any> =[];
+
+  constructor(private bookService: BookServiceService , public router : Router) { }
 
   ngOnInit(): void {
+    this.token = localStorage.getItem('token')
+    this.getwishlist();
   }
 
+  getwishlist(){
+    this.bookService.getWishlist(this.token).subscribe((data:any)=>{
+      this.wishlistBook=data['result']
+      console.log(this.wishlistBook)
+    })
+  }
+
+  removeItem=(data: any)=>{
+    console.log(data.product_id._id)
+    this.bookService.deleteWishlist(data.product_id._id, this.token).subscribe((response)=>{
+      console.log(response);
+      
+      this.getwishlist()
+    },
+    error => {
+      console.log(error);
+    });
+  }
+  homepage(){
+    this.router.navigate(['home']);
+  }
 }
