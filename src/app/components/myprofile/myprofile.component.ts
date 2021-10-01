c367bc2d2976f8b51b42f22c74eab997ed8f2b00
Nikita Rai <rainikita595@gmail.com>
2021-10-01 10:33:16 +0530

    [Nikita] Modify: function to retrive the data from json to form

--- src/app/components/myprofile/myprofile.component.ts
+++ src/app/components/myprofile/myprofile.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { BookServiceService } from 'src/app/services/book-service.service';
 import { CountService } from 'src/app/services/count.service';
-//import * as person from 'src/app/components/myprofile/myProfile.json'
 import person from 'src/app/components/myprofile/myProfile.json';
 import { HttpServiceService } from 'src/app/services/http-service.service';
 interface persons {  
@@ -25,14 +24,6 @@ interface persons {
 
 export class MyprofileComponent implements OnInit {
   
-  //personList:{full_name: String;  
-    //email: String;  
-    //password : String;
-    //phone : Number;
-    //address : String;
-    //city : String;
-    //State : String;
-    //Type : String;}[] = person;
     private example = person;
     personlist: persons[]=person;
     
@@ -41,22 +32,10 @@ export class MyprofileComponent implements OnInit {
   //person:string[];
 
   ngOnInit(): void {
-    //this.httpservice.get().subscribe((data:any)=>{
-    //  this.person=data['result']
-    //  console.log(this.person)
-   this.getProfile();
-   
-
+    
   }
   homepage(){
     this.router.navigate(['home']);
   }
-  //submit(){
-   //console.log(this.example);
-  //}
-  getProfile() {
-    this.httpservice.getProfileList().subscribe((res: any) => {
-      console.log('res', res);
-    })
-  }
+  
 }
--- src/app/services/http-service.service.ts
+++ src/app/services/http-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -34,4 +34,9 @@ export class HttpServiceService {
   getallBook(url: any) {
     return this.httpClient.get(this.url + url);
   }
+  public getJSON(): Observable<any> {
+    console.log("json data");
+    const url= 'src/app/components/myprofile/myProfile.json';
+    return this.httpClient.get(url);
+}
 }
